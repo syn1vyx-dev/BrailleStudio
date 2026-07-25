fcb40089c60ef77b18f72495c405729b70d8c8c2
syn1vyx-dev <syn1vyx@gmail.com>
2026-07-26 00:48:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 const grid = document.querySelector(".grid");
 
+let drawing = false;
+let eraseMode = false;
+
 for (let i = 0; i < 64; i++) {
     const dot = document.createElement("div");
 
     dot.className = "dot";
 
-    dot.onclick = () => {
-        dot.classList.toggle("active");
-    };
+    dot.addEventListener("mousedown", (event) => {
+        drawing = true;
+        eraseMode = event.shiftKey;
+
+        toggleDot(dot);
+    });
+
+    dot.addEventListener("mouseenter", (event) => {
+        if (drawing) {
+            eraseMode = event.shiftKey;
+
+            toggleDot(dot);
+        }
+    });
 
     grid.appendChild(dot);
+}
+
+
+document.addEventListener("mouseup", () => {
+    drawing = false;
+});
+
+
+function toggleDot(dot) {
+    if (eraseMode) {
+        dot.classList.remove("active");
+    } else {
+        dot.classList.add("active");
+    }
 }
