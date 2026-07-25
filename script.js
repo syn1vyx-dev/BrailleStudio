a684aa65a94e752e7b146f8d7ef17f03aeb32322
syn1vyx-dev <syn1vyx@gmail.com>
2026-07-26 00:43:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const grid = document.querySelector(".grid");
 
+for (let i = 0; i < 64; i++) {
+    const dot = document.createElement("div");
+
+    dot.className = "dot";
+
+    dot.onclick = () => {
+        dot.classList.toggle("active");
+    };
+
+    grid.appendChild(dot);
+}
